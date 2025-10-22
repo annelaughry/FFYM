c5db39ac8b0c6c1c74bf1cee6ced4a18a87ef438
Anne White <annelaughry@gmail.com>
2025-10-22 10:24:29 -0400

    change static files variable name

--- readingGuide/settings.py
+++ readingGuide/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 WHITENOISE_USE_FINDERS = True
 WHITENOISE_AUTOREFRESH = DEBUG
